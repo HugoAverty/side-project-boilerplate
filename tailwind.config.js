afaa352dc33b04f6237924739bf46c64c2b8b95a
Hugo Averty <hugoaverty@gmail.com>
2024-04-13 22:03:00 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'grey-blue': '#475467',
+        'light-blue': '#D0D5DD',
+        'dark': '#344054'
+      },
+      boxShadow: {
+        'light': '0px 1px 2px rgba(16, 24, 40, 0.05)',
+      }
+    },
+  },
+  plugins: [],
+}
